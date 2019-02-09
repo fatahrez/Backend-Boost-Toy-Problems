635a652669a6696c7cb47d6551503a6e9c9dcd84
fatahrez <fertahrez@gmail.com>
2019-02-10 00:51:45 +0300

    write the link of the kata and the its instruction

--- src/main/java/Fixme.java
+++ src/main/java/Fixme.java
@@ -1,2 +1,16 @@
+//link https://www.codewars.com/kata/fixme-static-electrickery
+
+//instruction
+//The code provided has a method plus100 which is supposed to return the number of the parameter incremented by 100.
+//
+//        But it's not working properly.
+//
+//        Task
+//        Fix the code so we can all go home early.
+//
+//        Restrictions
+//        * Do not modify the constructor
+//        * Do not modify the plus100 method
+//        * Do not modify the ONE_HUNDRED declaration
 public class Fixme {
 }
