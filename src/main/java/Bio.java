a32e5bfd3edc31b8a5f4813e68fc40805c2f19b5
fatahrez <fertahrez@gmail.com>
2019-02-23 22:23:11 +0300

    Paste link to kata in src/main/java/Bio.java

--- src/main/java/Bio.java
+++ src/main/java/Bio.java
@@ -1,3 +1,5 @@
+//link https://www.codewars.com/kata/dna-to-rna-conversion/train/java
+
 public class Bio {
     public String dnaToRna(String dna) {
         return dna.replaceAll("T", "U");  // Do your magic!
