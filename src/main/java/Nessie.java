424da150c64465838a86cc568caaa11815664771
fatahrez <fertahrez@gmail.com>
2019-02-23 23:39:21 +0300

    Create new class src/main/java/Nessie.java for new kata

---
+++ src/main/java/Nessie.java
@@ -0,0 +1,12 @@
+public class Nessie {
+    public static boolean isLockNessMonster(String s) {
+        //FIND THE LOCH NESS MONSTER. SAVE YOUR TREE FIDDY
+        Boolean ans = true;
+        if (s.indexOf("tree fiddy") != -1 || s.indexOf("$3.50") != -1) {
+            ans = true;
+        } else {
+            ans = false;
+        }
+        return ans;
+    }
+}
